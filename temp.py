d16aac8bb217d0f35a2a3561763ace9ec3eab4cc
rohanatanyway <rohan@any-way.ai>
2024-07-24 10:42:25 +0000

    add: testing script for both models, output samples for both models

--- temp.py
+++ temp.py
@@ -1,4 +1,5 @@
-from diffusers import DDIMScheduler, AutoencoderKL
+import torch
+from diffusers import DDIMScheduler, AutoencoderKL, DiffusionPipeline
 from transformers import CLIPTextModel, CLIPTokenizer
 from unet.interactdiffusion_unet_2d_condition import InteractDiffusionUNet2DConditionModel
 
@@ -12,11 +13,49 @@ tokenizer = CLIPTokenizer.from_pretrained(pretrained_model_name_or_path = "./tok
 unet = InteractDiffusionUNet2DConditionModel.from_pretrained("./unet", variant = "fp16")
 
 
-model = StableDiffusionInteractDiffusionPipeline(vae = vae, 
+model0 = StableDiffusionInteractDiffusionPipeline(vae = vae, 
                                                  text_encoder=text_encoder, 
                                                  tokenizer=tokenizer, 
                                                  unet=unet, 
                                                  scheduler = scheduler, 
-                                                 safety_checker=False, 
+                                                 safety_checker=None, 
                                                  feature_extractor=None)
 
+model0 = model0.to('cuda')
+images = model0(
+    prompt="a person is feeding a cat",
+    interactdiffusion_subject_phrases=["person"],
+    interactdiffusion_object_phrases=["cat"],
+    interactdiffusion_action_phrases=["feeding"],
+    interactdiffusion_subject_boxes=[[0.0332, 0.1660, 0.3359, 0.7305]],
+    interactdiffusion_object_boxes=[[0.2891, 0.4766, 0.6680, 0.7930]],
+    interactdiffusion_scheduled_sampling_beta=1,
+    output_type="pil",
+    num_inference_steps=50,
+    ).images
+
+images[0].save('out0.jpg')
+
+
+
+
+model1 = DiffusionPipeline.from_pretrained(
+    "interactdiffusion/diffusers-v1-2",
+    trust_remote_code=True,
+    variant="fp16", torch_dtype=torch.float16
+)
+
+model1 = model1.to('cuda')
+images = model1(
+    prompt="a person is feeding a cat",
+    interactdiffusion_subject_phrases=["person"],
+    interactdiffusion_object_phrases=["cat"],
+    interactdiffusion_action_phrases=["feeding"],
+    interactdiffusion_subject_boxes=[[0.0332, 0.1660, 0.3359, 0.7305]],
+    interactdiffusion_object_boxes=[[0.2891, 0.4766, 0.6680, 0.7930]],
+    interactdiffusion_scheduled_sampling_beta=1,
+    output_type="pil",
+    num_inference_steps=50,
+    ).images
+
+images[0].save('out1.jpg')
